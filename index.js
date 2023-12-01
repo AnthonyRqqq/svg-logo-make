524ec364d8031484aa6bbcb9de099612cf9aba9f
unknown <anthony_rosati@yahoo.com>
2023-11-30 23:42:10 -0600

    Adjusted code indentations for better structure

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ async function init() {
 
     // Collects user input
     const logo = await generateLogo.renderLogo();
-    
+
     // Utilizes user input to construct SVG shape
     const constructShape = await shapes.generateShape(logo);
 
-    // Creates the new logo.svg file in examples folder
+    // Creates the new logo.svg file in examples directory
     await writeToFile(constructShape);
 }
 
 // Starts program
 init();
 
-// Generates new logo.svg file in examples folder with finished logo
+// Generates new logo.svg file in examples folder with finished directory
 const writeToFile = (constructShape) => fs.writeFile('./examples/logo.svg', constructShape, (err) => {
     if (err) {
         console.error(err)

99a433f046c84b5a107e6c581dd129e1cd8f8338
unknown <anthony_rosati@yahoo.com>
2023-11-30 23:19:24 -0600

    Added code in shapes.js to construct SVG code using backticks

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Imports necessary modules
-const generateLogo = require('./lib/generateLogo.js')
-const shapes = require('./lib/shapes.js')
+const generateLogo = require('./lib/generateLogo.js');
+const shapes = require('./lib/shapes.js');
+const fs = require('fs');
 
+// Asynchronous function for running the program
 async function init() {
+    // Collects user input
     const logo = await generateLogo.renderLogo();
-    const renderShape = await shapes.generateShape(logo);
-    console.log(renderShape)
+    // Utilizes user input to construct SVG shape
+    const constructShape = await shapes.generateShape(logo);
+    console.log(constructShape);
 }
 
+// Starts program
 init();
+
+// const writeToFile = (constructShape) => {
+//     switch(constructShape) {
+//         case 'Square':
+//             fs.writeFile('new-SVG.svg')
+//     }
+// }
